c57cd3462c2d7f2b55383fa51ec3b48e1f0d5d83
yl_luminate <83196369+junwooYeom@users.noreply.github.com>
2022-09-10 22:08:27 +0900

    92341

--- src/main/kotlin/programmers/92341.kt
+++ src/main/kotlin/programmers/92341.kt
@@ -3,32 +3,9 @@ package programmers
 import kotlin.math.ceil
 
 /**
- * [링크] https://school.programmers.co.kr/learn/courses/30/lessons/92341?language=kotlin
+ * [92341] https://school.programmers.co.kr/learn/courses/30/lessons/92341?language=kotlin
  */
-
-fun main() {
-    val solutionClass = Solution()
-    val answer = solutionClass.solution(
-        intArrayOf(
-            180, 5000, 10, 600
-        ), arrayOf(
-            "05:34 5961 IN",
-            "06:00 0000 IN",
-            "06:34 0000 OUT",
-            "07:59 5961 OUT",
-            "07:59 0148 IN",
-            "18:59 0000 IN",
-            "19:09 0148 OUT",
-            "22:59 5961 IN",
-            "23:00 5961 OUT"
-        )
-    )
-    answer.forEach {
-        println(it)
-    }
-}
-
-class Solution {
+class `92341` {
     fun solution(fees: IntArray, records: Array<String>): IntArray {
         val answer: HashMap<String, Int> = hashMapOf()
         val cars: HashMap<String, Int> = hashMapOf()
