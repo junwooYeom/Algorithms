883adaa1379db06a271494afedf7ca00de5a3c07
yl_luminate <83196369+junwooYeom@users.noreply.github.com>
2022-10-15 20:51:23 +0900

    백준 17825

--- src/main/kotlin/backjoon/17825.kt
+++ src/main/kotlin/backjoon/17825.kt
@@ -2,6 +2,9 @@ package backjoon
 
 import java.util.StringTokenizer
 
+/**
+ * https://www.acmicpc.net/problem/17825
+ */
 private val board =
     intArrayOf(
         0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 0,
